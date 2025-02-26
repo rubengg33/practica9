dfadc96060912cc01b3b2f3ff899307454864f68
rubengg33 <135147356+rubengg33@users.noreply.github.com>
2025-02-26 22:45:05 +0100

    Solo falta ver el precio en el carrito

--- src/components/AddToCartButton.jsx
+++ src/components/AddToCartButton.jsx
@@ -4,30 +4,31 @@ import { addToCart } from "../lib/cart";
 const AddToCartButton = ({ course }) => {
     const handleAddToCart = async () => {
         const userId = localStorage.getItem("userId");
-        if (!userId) {
+        const token = localStorage.getItem("token");
+        
+        if (!userId || !token) {
           alert("Debes iniciar sesión para añadir cursos al carrito.");
           return;
         }
       
-        const added = await addToCart(userId, course);
-        
-        if (!added) {
-          return; // Si no se añade, no mostramos el mensaje
+        try {
+          const added = await addToCart(userId, course);
+          if (added === true) {
+            alert("Curso añadido al carrito!");
+          }
+        } catch (error) {
+          console.error("Error al añadir al carrito:", error);
         }
-      
-        alert("Curso añadido al carrito!");
-      };
-      
+    };
 
-
-  return (
-    <button
-      onClick={handleAddToCart}
-      className="p-2 bg-blue-500 text-white rounded-full hover:bg-blue-600"
-    >
-      <img src="../anadir-a-la-cesta.png" alt="Añadir" className="w-5 h-5" />
-    </button>
-  );
+    return (
+        <button
+          onClick={handleAddToCart}
+          className="p-2 bg-blue-500 text-white rounded-full hover:bg-blue-600"
+        >
+          <img src="../anadir-a-la-cesta.png" alt="Añadir" className="w-5 h-5" />
+        </button>
+    );
 };
 
 export default AddToCartButton;
